cb47fc9cdaa4c85e8b9f9e3ae3a518367c276db6
Abdullah Nabeel <abdullah_nabeel@Hotmail.com>
2015-07-02 09:24:26 +0500

    'FirstCommit'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Raaye::Application.config.secret_key_base = '6b3b35798c49a2775b732c40d6bf0e96a190a417eebdae94d29ca8fa44e19528757d19d8df98383d936ad25ddf6bdb677dd6dfd8cfa3bbece4b948a95c595c9b'
